5036b095ca269ca22934ea42bc2ca4de69e6e303
Tan Kenji <145350899+TostyBread@users.noreply.github.com>
2024-08-13 19:57:03 +0800

    Added more and fixing some
    -Added map flow collider to make the map less confusing
    -Fixed Panzer 4 turret
    -Finalised Briefing scene by adding EnemyInfo and background
    -Remove sceneloadertoo (as asycn doesnt do much better in my game)

--- Assets/Scripts/SceneManager/SceneLoader.cs
+++ Assets/Scripts/SceneManager/SceneLoader.cs
@@ -3,13 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject MainMenuUI;
+    public GameObject CreditsUI;
+
+    private void Start()
+    {
+        if (MainMenuUI != null && CreditsUI != null)
+        {
+            MainMenuUI.SetActive(true);
+            CreditsUI.SetActive(false);
+        }
+    }
+
     public void StartLevel1() // To Level 1
     {
         SceneManager.LoadScene("Level1");
     }
+
     public void StartBriefing() // To Briefing
     {
         SceneManager.LoadScene("Briefing");
+    }
+
+    public void CreditsButton() // To Credits during Main Menu
+    {
+        MainMenuUI.SetActive(false);
+        CreditsUI.SetActive(true);
+    }
+
+    public void MainMenuButton() // To Main Menu from Credits
+    {
+        MainMenuUI.SetActive(true);
+        CreditsUI.SetActive(false);
     }
 
     public void RestartGame() // Reloads current scene
